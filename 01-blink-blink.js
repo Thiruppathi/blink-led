d3b6490daf6e07ef80fb4d327d2ca647957cdb56
Thiru <thiru.androider@gmail.com>
2015-11-22 23:50:38 +0000

    Updated css; added package.son

--- 01-blink-blink.js
+++ 01-blink-blink.js
@@ -6,28 +6,34 @@ var five = require("johnny-five");
 var board = new five.Board()
 board.once('ready', function() {
 
-	var led = new five.Led(13);
-	var isOn = false;
-	this.repl.inject({
-		led: led
-	});
+  var led = new five.Led(13);
+  var isOn = false;
+  this.repl.inject({
+    led: led,
+		on: function() {
+			led.on();
+		},
+		off:function() {
+			led.off();
+		},
+		toggle: function() {
+			led.toggle();
+		}
+  });
 
+  var app = express();
+  app.use(ecstatic({
+    root: __dirname
+  }));
 
+  app.get('/toggleLed', function(req, res) {
+    led.toggle();
+    isOn = isOn ? false : true;
+    res.write(isOn ? 'ON' : 'OFF');
+    res.end();
+    console.log('At End ' + isOn);
+  });
 
-	var app = express();
-	app.use(ecstatic({
-		root: __dirname
-	}));
-
-	app.get('/toggleLed', function(req, res) {
-		led.toggle();
-		isOn = isOn ? false : true;
-		res.write(isOn ? 'ON' : 'OFF');
-		res.end();
-		console.log('At End ' + isOn);
-	});
-
-	http.createServer(app).listen(8080);
-
+  http.createServer(app).listen(9000);
 
 });
